cce6840e7990517ff7aab41ae02b3c4eeabf73c3
Hillel Arnold <helrond@hotmail.com>
2020-03-22 17:18:55 -0400

    remove schema and all artifacts

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-import jsonschema
-
-
-with open('schema.json', 'r') as sf:
-    schema = json.load(sf)
-    cls = jsonschema.validators.validator_for(schema)
-    cls.check_schema(schema)
